27f7a435a93ae47736426f158bae1e79dc25aa5e
torinosubako <ttcicore@gmail.com>
2021-11-06 00:38:48 +0900

    AWS IoT Core連携Test 第二段階

---
+++ AWStest2/AWStest2.ino
@@ -0,0 +1,148 @@
+/*
+   Project:AWS_TEST2
+   CodeName:Preparation_stage_001
+   Build:2021/11/06
+   Author:torinosubako
+   Status:Impractical
+*/
+
+#include <M5StickCPlus.h>
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+#include <PubSubClient.h>
+#include <Arduino_JSON.h>
+
+// Wi-Fi設定用基盤情報(2.4GHz帯域のみ)
+const char *ssid = //Your Network SSID//;
+const char *password = //Your Network Password//;
+
+// AWS IoT基幹情報
+const char* AWS_ENDPOINT = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.amazonaws.com";
+const int   AWS_PORT     = 8883;
+const char* PUB_TOPIC    = "test_core/fromDevice";
+const char* SUB_TOPIC    = "test_core/fromCloud";
+const char* CLIENT_ID    = "myM5StickCp";
+
+const char* ROOT_CA = R"EOF(-----BEGIN CERTIFICATE-----
+
+-----END CERTIFICATE-----)EOF";
+
+const char* CERTIFICATE = R"KEY(-----BEGIN CERTIFICATE-----
+
+-----END CERTIFICATE-----)KEY";
+
+const char* PRIVATE_KEY = R"KEY(-----BEGIN RSA PRIVATE KEY-----
+
+-----END RSA PRIVATE KEY-----)KEY";
+
+
+// MQTT設定
+#define QOS 1 
+WiFiClientSecure httpsClient;
+PubSubClient mqttClient(httpsClient);
+
+// デバイス制御
+RTC_DATA_ATTR static uint8_t seq;                     // RTC Memory シーケンス番号
+uint16_t Node_ID = 0000;
+unsigned long getDataTimer = 0;
+
+// Wi-Fi制御
+void setup_wifi() {
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  // ESP32でWiFiに繋がらなくなるときのための対策
+  WiFi.disconnect(true);
+  delay(1000);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+// AWSセットアップ
+void setup_AWS_MQTT(){
+  httpsClient.setCACert(ROOT_CA);
+  httpsClient.setCertificate(CERTIFICATE);
+  httpsClient.setPrivateKey(PRIVATE_KEY);
+  mqttClient.setServer(AWS_ENDPOINT, AWS_PORT);
+
+  Serial.println("Connecting MQTT...");
+  int retryCount = 0;
+  while (!mqttClient.connect(CLIENT_ID)){
+    Serial.println("Failed, state=" + String(mqttClient.state()));
+    if (retryCount++ > 3)
+      ESP.restart();
+    Serial.println("Try again in 5 seconds");
+    delay(5000);
+  }
+  Serial.println("Connected.");
+}
+
+// AWS-伝送用メッセージ生成
+String measure(){
+  JSONVar data;
+  data["Node_id"] = Node_ID;
+  data["Seq_no"] = seq;
+  data["Temp"] = 25.68;
+  data["Humi"] = 33.4;
+  data["WBGT"] = 11.4;
+  data["CO2"] = 514;
+  data["Press"] = 1024;
+  data["Node_Volt"] = M5.Axp.GetVBusVoltage();
+  data["Core_Volt"] = M5.Axp.GetVBusVoltage();
+  return JSON.stringify(data);
+}
+
+// AWS-MQTTアップロード
+void AWS_Upload(){
+  String payload = measure();
+  mqttClient.publish(PUB_TOPIC, payload.c_str());
+}
+
+void setup() {
+  M5.begin();
+  M5.Lcd.setRotation(1);
+  M5.Lcd.setTextFont(7);
+  setup_wifi();
+  // AWS関係関数
+  M5.Lcd.fillScreen(BLACK);
+  M5.Lcd.printf("%d", seq);
+  setup_AWS_MQTT();
+  AWS_Upload();
+  WiFi.disconnect();
+}
+
+void loop() {
+  M5.update();
+
+  //60秒毎に定期実行
+  auto now = millis();
+  M5.update();
+  if (now - getDataTimer >= 60000) {
+    getDataTimer = now;
+    seq++;
+    setup_wifi();
+    
+    // AWS関係関数
+    setup_AWS_MQTT();
+    AWS_Upload();
+
+    M5.Lcd.fillScreen(BLACK);
+    M5.Lcd.setCursor(5, 5);
+    M5.Lcd.printf("%d", seq);
+
+    Serial.printf("Now_imprinting\r\n");
+    //WiFi切断
+    WiFi.disconnect();
+    
+  }
+}
